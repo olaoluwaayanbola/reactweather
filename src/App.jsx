9942b672639ee11226524d6b2008756eb5ab4743
Ayanbola <olaoluwaayanbola@gmail.com>
2022-09-21 23:41:32 +0100

    new ranch commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [input, setInput] = useState("")
   const [weatheria, setweather] = useState({})
   const [bool, setbool] = useState(true)
+  const [random, setRandom] = useState(0)
 
   // handle form
   const handleinput = (e) => {
@@ -16,35 +17,35 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault()
   }
-  
-  // fetch backgroundImage
+
+  // fetching backgroundImage from unsplash
   const fetch = async () => {
     try {
-      const data = await axios.get(`https://api.unsplash.com/search/photos?page=1&per_page=1&query=${input}&client_id=ElihHhCfiATgmQsBphWUw24mtbALKw_I0r7jYaMqArY`)
+      const data = await axios.get(`https://api.unsplash.com/search/photos?page=1&per_page=6&query=${input}&client_id=ElihHhCfiATgmQsBphWUw24mtbALKw_I0r7jYaMqArY`)
       setState(data.data.results)
     } catch (error) {
       console.log("=>" + error)
     }
-
   }
 
-  // weatheria get data
-  const options = {
-    method: 'GET',
-    url: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
-    params: { q: 'London', days: '3' },
-    headers: {
-      'X-RapidAPI-Key': '862b001e0cmsh956a88122e9550cp19f1a9jsn71cb258cff8a',
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
-  };
 
+  // getting weather data from weatheria 
   const weather = async () => {
+    const options =
+    {
+      method: 'GET',
+      url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+      params: { city: `${input}` },
+      headers: {
+        'X-RapidAPI-Key': '862b001e0cmsh956a88122e9550cp19f1a9jsn71cb258cff8a',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+      }
+    }
     try {
       const data = await axios.request(options)
-      setweather(data.data)
+      setweather(data)
     } catch (error) {
-      console.log("=>" + error)
+      console.log("weather api =>" + error)
     }
   }
 
@@ -58,25 +59,40 @@ function App() {
     <div className="App_Container">
       <div className="Weather_Main">
         <div className="Left">
-          <Left data={state} condition={bool} />
+          <Left
+            data={state}
+            weather={weatheria}
+            condition={bool}
+            random={random}
+            input={input}
+          />
         </div>
         <div className="Right">
-          <div class="Input_Box">
+          <div className="Input_Box">
             <form onSubmit={handleSubmit}>
               <input
                 type="text"
                 value={input}
                 onChange={handleinput}
                 placeholder="Region"
-                class="Region_Input"
+                className="Region_Input"
+                autoFocus="true"
               />
-              <button class="Button" onClick={() => { fetch(); setbool(false) }} >
+              <button className="Button" onClick={
+                () => {
+                  fetch();
+                  weather();
+                  setbool(false);
+
+                  setRandom(Math.floor(Math.random() * 5));
+                }
+              } >
                 GO
               </button>
             </form>
           </div>
           <div className="Right_Box">
-            <Right data={state[0]} />
+            <Right data={weatheria.data} />
           </div>
         </div>
       </div>
